93aeddbdde06c3a24a9fea51fc317b7200715a0a
marcelldac <devmarcell@protonmail.com>
2024-04-21 13:19:22 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,9 +2,8 @@ import type { HttpRequest, HttpResponse } from '../protocols/http'
 import type { Controller } from '../protocols/controller'
 import type { EmailValidator } from '../protocols/email-validator'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { InvalidParamError } from '../errors/invalid-param-errror'
-import { ServerError } from '../errors/server-error'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
 
@@ -26,10 +25,7 @@ export class SignUpController implements Controller {
       }
       return { body: 'Ok', statusCode: 200 }
     } catch (err) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
